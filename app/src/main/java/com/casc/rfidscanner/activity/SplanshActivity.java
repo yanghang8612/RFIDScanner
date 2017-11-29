6cbafe6fcdc697cbc7ad94f41cf1b21d2ef5d0f5
yab <1576702791@qq.com>
2017-11-29 17:00:38 +0800

    基本页面及跳转

---
+++ app/src/main/java/com/casc/rfidscanner/activity/SplanshActivity.java
@@ -0,0 +1,68 @@
+package com.casc.rfidscanner.activity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+
+import com.casc.rfidscanner.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplanshActivity extends BaseActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splansh);
+
+        //获取sp登录信息
+        Boolean isLogin=false;
+
+        SharedPreferences sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE); //私有数据
+        String  name = sharedPreferences.getString("name","");
+        String pwd = sharedPreferences.getString("pwd","");
+        if (name!=""&&isLeagal(name,pwd))
+            isLogin = true;
+
+        mHandler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        mHandler.postDelayed(new DelayRunnable(this,isLogin), 2000);
+
+    }
+    static class DelayRunnable implements Runnable {
+        Context context;
+        Boolean isLogin;
+        public DelayRunnable(Context context,Boolean isLogin){
+            this.context = context;
+            this.isLogin = isLogin;
+
+        }
+        @Override
+        public void run() {
+            if (isLogin)
+                context.startActivity(new Intent(context, MainActivity.class));
+            else
+                context.startActivity(new Intent(context,LoginActivity.class));
+            ((Activity) context).finish();
+        }
+    }
+
+    public Boolean isLeagal(String name , String pwd)
+    {
+        return true;
+    }
+}
